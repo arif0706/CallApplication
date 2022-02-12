e417e3c8c6c745b216bb5e6a51ce86fd13f1310c
arif0706 <mohammedarif0706@gmail.com>
2022-02-13 00:08:54 +0530

    Some changes then updated

--- app/src/main/java/com/example/callapplication/MainActivity.kt
+++ app/src/main/java/com/example/callapplication/MainActivity.kt
@@ -6,6 +6,12 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
+import com.github.javiersantos.appupdater.AppUpdater
+import com.github.javiersantos.appupdater.AppUpdaterUtils
+import com.github.javiersantos.appupdater.enums.AppUpdaterError
+import com.github.javiersantos.appupdater.enums.Display
+import com.github.javiersantos.appupdater.enums.UpdateFrom
+import com.github.javiersantos.appupdater.objects.Update
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.initialize
 import kotlinx.android.synthetic.main.activity_main.*
@@ -31,6 +37,24 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("username",userName)
             startActivity(intent)
         }
+
+       val appUpdaterUtils= AppUpdaterUtils(this)
+           .setUpdateFrom(UpdateFrom.GITHUB)
+           .setGitHubUserAndRepo("arif0706","CallApplication")
+           .withListener(object :AppUpdaterUtils.UpdateListener{
+               override fun onSuccess(update: Update?, isUpdateAvailable: Boolean?) {
+                   println("updater"+ update?.latestVersion+ update?.urlToDownload + isUpdateAvailable)
+               }
+
+               override fun onFailed(error: AppUpdaterError?) {
+                    println("error"+error?.name)
+               }
+           })
+
+        appUpdaterUtils.start()
+
+
+
     }
 
     private fun askPermission() {
